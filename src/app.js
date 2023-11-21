191761ea349bd10a108cb1f9f7ec4462ee3cd54f
Baibhav Kumar <baibhav.kr73@gmail.com>
2023-11-21 15:47:17 +0530

    Wrote register route without logic inside this route

--- src/app.js
+++ src/app.js
@@ -5,15 +5,21 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  }),
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    }),
 );
 
 app.use(express.json({ limit: "16kb" })); //used to convert the json data into js objects and vise-versa.
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //After using this middleware, We don't need to use body-parser
 app.use(express.static("public")); //for serving the static file
 app.use(cookieParser());
+
+//routes import
+
+import userRouter from "./routes/user.route.js";
+//routes declarations
+app.use("/api/v1/users", userRouter);
 
 export { app };
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const registerUser = (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "User registered successfully",
+    });
+};
+
+export { registerUser };
